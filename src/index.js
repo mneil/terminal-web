32034f33ba7a870a7889cce6caf0ac605461b43c
Michael Neil <mneil@mneilsworld.com>
2022-05-06 18:20:24 -0700

    Slightly smarter terminal history

--- src/index.js
+++ src/index.js
@@ -6,16 +6,23 @@ lg.onRuntimeInitialized = () => {
   console.log("init");
 
   const term = new Terminal(document.getElementById("terminal"));
+  term.on("command", (evt) => {
+    if (evt.app === "git") {
+      console.log("calling git with", evt.args);
+      lg.callMain(evt.args);
+    }
+  });
   const FS = lg.FS;
   const MEMFS = FS.filesystems.MEMFS;
 
-  FS.mkdir("/working");
-  FS.mount(MEMFS, {}, "/working");
-  FS.chdir("/working");
+  FS.mkdir("/app");
+  FS.mount(MEMFS, {}, "/app");
+  FS.chdir("/app");
 
   FS.writeFile("/home/web_user/.gitconfig", "[user]\n" + "name = Test User\n" + "email = test@example.com");
 
   // clone a repository from github
-  lg.callMain(["clone", "https://github.com/torch2424/made-with-webassembly.git", "made-with-webassembly"]);
-  console.log(FS.readdir("made-with-webassembly"));
+  //lg.callMain(["clone", "https://github.com/torch2424/made-with-webassembly.git", "made-with-webassembly"]);
+  //console.log(FS.readdir("made-with-webassembly"));
+  //FS.chdir("made-with-webassembly");
 };
--- src/terminal.js
+++ src/terminal.js
@@ -1,13 +1,24 @@
 require("xterm/css/xterm.css");
 const xterm = require("xterm");
+const EventEmitter2 = require("eventemitter2");
 
-class Terminal {
+/**
+ * Terminal class creates an instance of xtermjs. Handles keyboard input, history, etc...
+ *
+ * Emits more complete events than the addon interfaces gives you access to for building integrations.
+ * Emits:
+ *  - command: When user enters a command. Get the full current line the user entered
+ */
+class Terminal extends EventEmitter2 {
   #maxHistory = 1000;
+  #hitoryPos = 0;
+  #tempHistory = "";
   #newline = "$ ";
   #term;
   line = "";
   history = [];
   constructor(el) {
+    super();
     this.#term = new xterm.Terminal({
       cursorBlink: "block",
     });
@@ -21,12 +32,25 @@ class Terminal {
       if (evt.domEvent.code === "Backspace") {
         return this.backspace();
       }
+      if (evt.domEvent.code === "ArrowUp") {
+        return this.toHistory(-1);
+      }
+      if (evt.domEvent.code === "ArrowDown") {
+        return this.toHistory(1);
+      }
+      console.log(evt);
       this.line += evt.key;
       this.#term.write(evt.key);
     });
   }
 
   enter() {
+    // reset history scrolling
+    this.#hitoryPos = 0;
+    this.#tempHistory = "";
+    // emit the command
+    const [app, ...args] = this.line.split(" ");
+    this.emit("command", { app, args, line: this.line });
     // store non-empty lines in this.history
     this.line.trim() && this.history.push(this.line);
     // FIFO queue hitory items after max this.history
@@ -36,12 +60,44 @@ class Terminal {
     this.#term.writeln("");
     this.#term.write(this.#newline);
   }
+  /**
+   * toHistory takes an integer -1 or 1 to advance or rewind
+   * the history. This method clears the current lines and
+   * retrieves commands from history.
+   *
+   * @param {*} direction
+   */
+  toHistory(direction) {
+    if (!this.#tempHistory) {
+      this.#tempHistory = this.line;
+    }
+    for (let i = 0; i < this.line.length; i++) {
+      this.backspace(); // clear current line
+    }
+    this.#hitoryPos += direction;
+    if (this.#hitoryPos < -this.history.length) {
+      // at the beginning
+      this.#hitoryPos = -this.history.length;
+    }
+    if (this.#hitoryPos > -1) {
+      // at the end - restore user's temporary work
+      this.#hitoryPos = -1;
+      this.line = this.#tempHistory;
+      return this.#term.write(this.line);
+    }
+    this.line = this.history[this.history.length + this.#hitoryPos];
+    this.#term.write(this.line);
+  }
 
   backspace() {
     if (this.line) {
       this.line = this.line.slice(0, this.line.length - 1);
       this.#term.write("\b \b");
     }
+  }
+
+  loadAddon(addon) {
+    this.#term.loadAddon(addon);
   }
 }
 
